a74344e38091524ad2733324c52612a6365a05ef
Rachid_Abdellaoui <rachidweb4@gmail.com>
2020-11-18 15:53:49 +0100

    calculette

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # calculette
+
++ Projet Solo
+
++ Date du 16/11/2020 au 18/11/2020 19:00
+
+## Objectif
+
+Générer une mise en page de la calculatrice avec un écran sur le dessus et tous les boutons de chiffres (0 à 9), les 
+
+touches de commande ( +, -, x, /), un bouton pour calculer le résultat ( =) et un bouton pour effacer l'écran ( C).
+
+## Langage
+
+:white_check_mark: HTML5 (vide)
+
+:white_check_mark: CSS3
+
+:white_check_mark: Javascript
+
+
--- calculator.js
+++ calculator.js
@@ -1,87 +1,114 @@
-const nbr = [{
-      valeur: "(",
-   },
-   {
-      valeur: ")",
-   },
-   {
-      valeur: "%",
-   },
-   {
-      valeur: "AC",
-   },
-   {
-      valeur: 1,
-   },
-   {
-      valeur: 2,
-   },
-   {
-      valeur: 3,
-   },
-   {
-      valeur: "+",
-   },
-   {
-      valeur: 4,
-   },
-   {
-      valeur: 5,
-   },
-   {
-      valeur:6,
-   },
-   {
-      valeur:"-",
-   },
-   {
-      valeur:7,
-   },
-   {
-      valeur:8,
-   },
-   {
-      valeur:9,
-   },
-   {
-      valeur:"*",
-   },
-   {
-      valeur:".",
-   },
-   {
-      valeur:0,
-   },
-   {
-      valeur:"=",
-   },
-   {
-      valeur:"/",
-   },
+ const nbr = [{
+         valeur: "(",
+     },
+     {
+         valeur: ")",
+     },
+     {
+         valeur: "%",
+     },
+     {
+         valeur: "AC",
+     },
+     {
+         valeur: 1,
+     },
+     {
+         valeur: 2,
+     },
+     {
+         valeur: 3,
+     },
+     {
+         valeur: "+",
+     },
+     {
+         valeur: 4,
+     },
+     {
+         valeur: 5,
+     },
+     {
+         valeur: 6,
+     },
+     {
+         valeur: "-",
+     },
+     {
+         valeur: 7,
+     },
+     {
+         valeur: 8,
+     },
+     {
+         valeur: 9,
+     },
+     {
+         valeur: "*",
+     },
+     {
+         valeur: ".",
+     },
+     {
+         valeur: 0,
+     },
+     {
+         valeur: "=",
+     },
+     {
+         valeur: "/",
+     },
 
 
-]
+ ]
+
+
+//  const colors = ['red', 'blue', 'yellow', 'pink', 'lime']
+//  document.body.addEventListener('keypress', function () {
+//      const randomColor = Math.floor(Math.random() + color.length)
+//      document.body.style.background = colors[randomColor]
+//  })
+ document.querySelector("main").innerHTML = ''
+ 
+
+
+ const monContainer = document.querySelector("main");
+
+
+ const calculette = document.createElement('div');
+ calculette.className = "contenair";
+ monContainer.appendChild(calculette);
 
 
 
-document.querySelector("main").innerHTML = ''
+ const affichage = document.createElement('div');
+ affichage.className = "aff";
+ calculette.appendChild(affichage);
+
+ function safeEval(str) {
+     return Function('return ' + str)()
+ }
+
+ for (let element of nbr) {
+     const btn = document.createElement('button');
+     btn.textContent = element.valeur;
+     calculette.appendChild(btn);
+
+     btn.addEventListener("click", function () {
+
+         if (element.valeur == "AC") {
+             affichage.textContent = "";
 
 
-const monContainer = document.querySelector("main");
+         } else if (element.valeur == "=") {
+             let reponse = safeEval(affichage.textContent);
+             affichage.textContent = reponse
+            
 
+         } else {
+             affichage.textContent += element.valeur
+         }
 
-const calculette = document.createElement('div');
-calculette.className = "contenair";
-monContainer.appendChild(calculette);
+     })
 
-const resultat = document.createElement('div');
-resultat.className = "output";
-calculette.appendChild(resultat);
-
-for (let element of nbr) {
-   const btn = document.createElement('button');
-   btn.textContent = element.valeur;
-   calculette.appendChild(btn);
-
-}
-let n = document.getElementById
+ }
